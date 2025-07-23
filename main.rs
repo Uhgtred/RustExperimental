6f051b2859bb775200c64e77a5d473b051d345de
uhgtred <markus_koesters@gmx.de>
2025-07-23 22:40:56 +0200

    Add comments to explain main function, variables, and loops.

--- main.rs
+++ main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    /*
+    This is the main-function of my Rust-Experimental repository.
+    This is for learning rust only.
+    Nothing meaningful is going to happen in this repository!
+     */
     let string_var = "Hello, world!";
     println!("{}", string_var);
     // this is declaring and defining an integer.
@@ -10,7 +15,9 @@ fn main() {
     for counter in 0..MY_INT {
         println!("This is the result of a loop in the {} run. 2x counter is: {}",counter , counter + counter);
     }
+
     let mut counter: i8 = 3;
+    // This is a while-loop.
     while counter > 0 {
         println!("Running a while-loop. Counter is: {}", counter);
         counter -= 1;
