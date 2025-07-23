05886dfb0e1e0d4c20a1794e9a97a671f6d21159
uhgtred <markus_koesters@gmx.de>
2025-07-23 22:36:52 +0200

    Initialize Rust project with basic setup files and IDE configurations.

---
+++ main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let string_var = "Hello, world!";
+    println!("{}", string_var);
+    // this is declaring and defining an integer.
+    const MY_INT: i8 = 3;
+    let mut my_variable_unsigned_int: u8 = 10;
+    my_variable_unsigned_int = my_variable_unsigned_int - 10;
+    println!("The unsigned integer has been reduced to:\t{}", my_variable_unsigned_int);
+    // this is a loop
+    for counter in 0..MY_INT {
+        println!("This is the result of a loop in the {} run. 2x counter is: {}",counter , counter + counter);
+    }
+    let mut counter: i8 = 3;
+    while counter > 0 {
+        println!("Running a while-loop. Counter is: {}", counter);
+        counter -= 1;
+    }
+}
